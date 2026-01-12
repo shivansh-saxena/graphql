a584af2f6e66bb5b5f78c07cadc141bfc51657cd
Shivansh Saxena <shivanshsaxena08@gmail.com>
2026-01-12 21:40:23 +0530

    Converting Rest API to GraphQL: In ProductController.class: 1. Remove @RestController & @RequestMapping and replace with @Controller 2. Remove @GetMapping and replace with @QueryMapping 3. Remove @PathVariable and replace with @Argument
    Define schema.graphqls file:
    1. In type define the object and DTOs
    2. In query define the methods with arguments

--- src/main/java/org/shiv/graphql/resource/ProductController.java
+++ src/main/java/org/shiv/graphql/resource/ProductController.java
@@ -3,29 +3,31 @@ package org.shiv.graphql.resource;
 import org.shiv.graphql.entity.Product;
 import org.shiv.graphql.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 //@RestController
 //@RequestMapping("/products")
+@Controller
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @GetMapping
+//    @GetMapping
+    @QueryMapping
     public List<Product> getAllProducts() {
         List<Product> tmp =  productService.getAllProducts();
         System.out.println(tmp);
         return tmp;
     }
 
-    @GetMapping("/{category}")
-    public List<Product> getProductsByCategory(@PathVariable String category) {
+//    @GetMapping("/{category}")
+    @QueryMapping
+    public List<Product> getProductsByCategory(/*@PathVariable*/@Argument String category) {
         return productService.getProductsByCategory(category);
     }
 }
